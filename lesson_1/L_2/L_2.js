7d4bb0db54b266a0f4f79652e98a1313980e5733
Cooskin <1575437818@qq.com>
2019-08-17 22:22:27 +0800

    L2

--- lesson_1/L_2/L_2.js
+++ lesson_1/L_2/L_2.js
@@ -1,15 +1,63 @@
-window.onload = function () {
+window.onload = function() {
     var oDiv = document.getElementById('div1');
     var oText = document.getElementById('text');
     var oBtn = document.getElementById('btn');
-    var oSpan = document.getElementById('span1');
+    var oImg = document.getElementById('img');
+    var ali = document.getElementsByTagName('li');
+    // var con = document.getElementById('con_l');
+    // var img = document.getElementById('img_l');
 
+
+    var url = true;
+
+    oImg.style.background = 'url(./img/img_1.gif)';
+
+    // oSpan.style.background = 'url(./img/img_1.gif)';
+    oImg.onclick = function() {
+        if (url) {
+            oImg.style.background = 'url(./img/img_2.gif)';
+            url = false;
+        } else {
+            oImg.style.background = 'url(./img/img_1.gif)';
+            url = true;
+        }
+    }
+
+    oBtn.onclick = function() {
+
+        if (oText.value == "") {
+            alert('请输入后再发送')
+            return false;
+        }
+
+
+        if (url) {
+
+            ali[0].innerHTML += '<div class="background_l"></div>' + '<div class="con_l">' + oText.value + '</div>' + '<br/>';
+            oText.value = "";
+            img.src = oImg.src;
+        } else {
+            ali[0].innerHTML += '<div class="background_r"></div>' + '<div class="con_l">' + oText.value + '</div>' + '<br/>';
+            oText.value = "";
+            img.src = oImg.src;
+        }
+
+
+    }
+
+
+
+
+
+
+    /*
     oBtn.onclick = function () {
         var msg = oText.value;
         sendMsgA(msg)
     }
+    */
 };
-
+/*
 function sendMsgA(msg) {
     var oDiv = document.getElementById('div1');
     var msgA = document.createElement("div");
@@ -20,3 +68,4 @@ function sendMsgA(msg) {
     msgA.innerHTML = spr;
     oDiv.appendChild(msgA)
 }
+*/
