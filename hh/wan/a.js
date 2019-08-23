c1d2fae8d0d1c1995d154e6473f7cd9d74dd3458
Cooskin <1575437818@qq.com>
2019-08-23 23:36:06 +0800

    hh

--- hh/wan/a.js
+++ hh/wan/a.js
@@ -1,10 +1,30 @@
 window.onload = function() {
-    var aInp = document.getElementsByTagName('input');
 
-    aInp[0].onclick = function() {
+    var oUl = document.getElementById('list');
+    // oLi = document.getElementById('lis');
 
-        for (var i = 0; i < 10; i++) {
-            document.body.innerHTML += '<div>' + i + '</div>'
+    var aH2 = document.getElementsByTagName('h2'),
+        aUl = oUl.getElementsByTagName('ul');
+
+
+    for (let i = 0; i < aH2.length; i++) {
+
+        aH2[i].key = true;
+
+        aH2[i].onclick = function() {
+
+            if (aH2[i].key == true) {
+                aUl[i].style.display = 'block';
+                this.style.backgroundImage = 'url(./img/ico2.gif)';
+                this.key = false;
+            } else {
+                aUl[i].style.display = 'none';
+                this.style.backgroundImage = 'url(./img/ico1.gif)';
+                this.key = true;
+            }
+
         }
+
     }
+
 }
